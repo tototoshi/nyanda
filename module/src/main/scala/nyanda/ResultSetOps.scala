5ebd0cd3c9ee3aab9f152a6aaf8122973a3aed8b
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2022-01-05 23:30:46 +0900

    Refactoring

--- module/src/main/scala/nyanda/ResultSetOps.scala
+++ module/src/main/scala/nyanda/ResultSetOps.scala
@@ -5,27 +5,19 @@ import scala.collection.mutable.ListBuffer
 
 class ResultSetOps(rs: ResultSet):
 
+  private def it: Iterator[ResultSet] = Iterator.unfold(rs) { rs => if (rs.next()) Some((rs, rs)) else None }
+
   def option[A](reader: ResultSetReader[A]): ResultSetReader[Option[A]] =
     new ResultSetReader[Option[A]] {
       def read(rs: ResultSet): Option[A] =
-        if (rs.next()) {
-          val result = reader.read(rs)
-          if (rs.next()) {
-            throw new TooManyRowException("More than single record are found")
-          } else {
-            Some(result)
-          }
-        } else {
-          None
+        it.take(2).toList match {
+          case Nil => None
+          case h :: Nil => Some(reader.read(h))
+          case _ => throw new TooManyRowException("More than single record are found")
         }
     }
 
   def seq[A](reader: ResultSetReader[A]): ResultSetReader[Seq[A]] =
     new ResultSetReader[Seq[A]] {
-      def read(rs: ResultSet): Seq[A] =
-        val buffer: ListBuffer[A] = ListBuffer.empty[A]
-        while (rs.next()) {
-          buffer += reader.read(rs)
-        }
-        buffer.toSeq
+      def read(rs: ResultSet): Seq[A] = it.map(reader.read).toSeq
     }
